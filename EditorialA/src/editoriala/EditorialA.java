7d9bf73a8be7367bb7b26faaf0c38d6483c2c7b5
Sergio Santana Cardenas <67561523+AnCheck14@users.noreply.github.com>
2022-04-29 23:07:41 -0500

    Add files via upload

---
+++ EditorialA/src/editoriala/EditorialA.java
@@ -0,0 +1,20 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package editoriala;
+
+import vista.Bienvenida;
+
+/**
+ *
+ * @author sergi
+ */
+public class EditorialA {
+    public static void main(String[] args) {
+        Bienvenida b1 = new Bienvenida();
+        b1.setVisible(true);
+    }
+    
+}
